b12bdf751fa2b52f706aec07f194cd0305cdd783
Shubhi Gupta <79734129+ShubhiGupta15@users.noreply.github.com>
2022-08-21 09:48:46 +0530

    Update ThreadPoolExecutor.py

--- concurrent.futures/ThreadPoolExecutor.py
+++ concurrent.futures/ThreadPoolExecutor.py
@@ -1,1 +1,17 @@
+import concurrent.futures
+import time
 
+def calc_cube(numbers):
+    print("calculate cube of numbers")
+    for n in numbers:
+        time.sleep(0.2)
+        print('cube:',n*n*n)
+
+arr = [2,3,8,9]
+if _name_ == "_main_":
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        start_time = time.perf_counter()
+        result = list(executor.map(calc_cube, arr))
+        finish_time = time.perf_counter()
+    print(f"Program finished in {finish_time-start_time} seconds")
+    print(result)
